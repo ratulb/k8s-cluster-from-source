93d564b25a49ef7e59040e247bb4207e071a023c
Ratul Buragohain <ratul75@hotmail.com>
2020-10-07 22:16:57 +0000

    Removing and adding stuff

--- systemd/etcd/start-etcd-servers.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#Reload daemons and restart service
-
-for instance in etcd-1 etcd-2 etcd-3; do
-  lxc exec ${instance} -- systemctl daemon-reload
-  lxc exec ${instance} -- systemctl enable etcd
-  lxc exec ${instance} -- systemctl start etcd
-done
