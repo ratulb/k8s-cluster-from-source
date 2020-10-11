f592d33614426117665ccec13b3905e1742d8407
Ratul <ratul75@hotmail.com>
2020-10-11 19:41:53 +0530

    Update gen-kubelet-configs.sh

--- scripts/kubelet/gen-kubelet-configs.sh
+++ scripts/kubelet/gen-kubelet-configs.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 #Generate each worker nodes kube config files
-
 {
 . ../run-as-root.sh
-
 WORKERS=
-
 if [ $# -eq 0 ];
   then
     WORKERS="worker-1 worker-2 worker-3"
@@ -15,13 +12,13 @@ if [ $# -eq 0 ];
     WORKERS=$@
     echo "Setting up for $WORKERS"
 fi
-
 #The load balancer IP fronting the kube-apiservers
 KUBERNETES_PUBLIC_ADDRESS=$(lxc list | grep loadbalancer | awk '{print $6}')
 
 GEN_CERTS_PATH=../../certificates/generated
 GEN_CONFIG_PATH=../../kubeconfigs
 mkdir -p ${GEN_CONFIG_PATH}
+
 for instance in $WORKERS; do
   kubectl config set-cluster kubernetes-cluster-ground-up-from-sources \
     --certificate-authority=${GEN_CERTS_PATH}/ca.pem \
@@ -39,7 +36,6 @@ for instance in $WORKERS; do
     --cluster=kubernetes-cluster-ground-up-from-sources \
     --user=system:node:${instance} \
     --kubeconfig=${GEN_CONFIG_PATH}/${instance}.kubeconfig
-
   kubectl config use-context default --kubeconfig=${GEN_CONFIG_PATH}/${instance}.kubeconfig
 done
 }
