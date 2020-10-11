f1cb9138981bf79adc5ac111845a5ef20213f2b8
Ratul <ratul75@hotmail.com>
2020-10-11 19:16:28 +0530

    Update distribute-kube-controller-manager-certs.sh

--- scripts/distribute-kube-controller-manager-certs.sh
+++ scripts/distribute-kube-controller-manager-certs.sh
@@ -4,10 +4,17 @@
 #Create required directories if needed
 #Note: Service account cert and keys are also getting copied via this script
 cd ../certificates/
-for instance in master-1 master-2 master-3; do
+MASTERS=
+if [ $# -eq 0 ];
+  then
+    MASTERS="master-1 master-2 master-3"
+  else
+    MASTERS=$@
+fi
+echo "Setting up $MASTERS"
+for instance in $MASTERS; do
   lxc exec ${instance} -- mkdir -p /var/lib/kubernetes/
   lxc file push ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem \
   service-account-key.pem ${instance}/var/lib/kubernetes/
 done
-
 cd -
