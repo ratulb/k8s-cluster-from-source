3564b91c89f86cbb93e4eff31767124f5a80dcce
Ratul <ratul75@hotmail.com>
2020-10-11 19:44:19 +0530

    Update reboot-kubelet-boxes.sh

--- scripts/kubelet/reboot-kubelet-boxes.sh
+++ scripts/kubelet/reboot-kubelet-boxes.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-
 #Reload daemons and restart  kubelet service
-
 . ../run-as-root.sh
-
 WORKERS=
-
 if [ $# -eq 0 ];
   then
     WORKERS="worker-1 worker-2 worker-3"
@@ -14,7 +10,6 @@ if [ $# -eq 0 ];
     WORKERS=$@
     echo "Setting up for $WORKERS"
 fi
-
 for instance in $WORKERS; do
   lxc exec ${instance} reboot
 done
