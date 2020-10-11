628e7e2eed4c375054f489ce94666b2bfa3473e4
Ratul <ratul75@hotmail.com>
2020-10-11 19:34:12 +0530

    Update install-os-dependencies.sh

--- scripts/kubelet/install-os-dependencies.sh
+++ scripts/kubelet/install-os-dependencies.sh
@@ -3,7 +3,6 @@
 . ../run-as-root.sh
 
 WORKERS=
-
 if [ $# -eq 0 ];
   then
     WORKERS="worker-1 worker-2 worker-3"
@@ -12,13 +11,9 @@ if [ $# -eq 0 ];
     WORKERS=$@
     echo "Setting up for $WORKERS"
 fi
-
 for instance in $WORKERS; do
-
-#lxc exec ${instance} -- apt-get update
-#lxc exec ${instance} -- apt-get install -y libseccomp2 socat conntrack ipset
+ lxc exec ${instance} -- apt-get update
+ lxc exec ${instance} -- apt-get install -y libseccomp2 socat conntrack ipset
 echo "Setting up os dependencies in $instance"
-
 done
-
 printf "Done installing os dependencies..."
