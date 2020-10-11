700ed788283f3269811892d60ee0fa203c3ea8ec
Ratul <ratul75@hotmail.com>
2020-10-11 19:19:28 +0530

    Update distribute-kube-scheduler-config.sh

--- scripts/distribute-kube-scheduler-config.sh
+++ scripts/distribute-kube-scheduler-config.sh
@@ -2,9 +2,15 @@
 
 #Distribute the kube-scheduler config files to master nodes
 #Create required directories if needed
-
-
-for instance in master-1 master-2 master-3; do
+MASTERS=
+if [ $# -eq 0 ];
+  then
+    MASTERS="master-1 master-2 master-3"
+  else
+    MASTERS=$@
+fi
+echo "Setting up $MASTERS"
+for instance in $MASTERS; do
   lxc exec ${instance} -- mkdir -p /var/lib/kubernetes/
   lxc file push ../kubeconfigs/kube-scheduler.kubeconfig ${instance}/var/lib/kubernetes/
   echo "Copied the kube-scheuler.kubeconfig to ${instance}"
