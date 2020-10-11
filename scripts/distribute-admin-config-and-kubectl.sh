70049aed50f1e8adef2048eb765dbdbaf1466c18
Ratul <ratul75@hotmail.com>
2020-10-11 19:09:06 +0530

    Update distribute-admin-config-and-kubectl.sh

--- scripts/distribute-admin-config-and-kubectl.sh
+++ scripts/distribute-admin-config-and-kubectl.sh
@@ -2,9 +2,15 @@
 
 #Distribute the admin config file and kubectl binary to master nodes
 #Create required directories if needed
-
-
-for instance in master-1 master-2 master-3; do
+MASTERS=
+if [ $# -eq 0 ];
+  then
+    MASTERS="master-1 master-2 master-3"
+  else
+    MASTERS=$@
+fi
+echo "Setting up $MASTERS"
+for instance in $MASTERS; do
   lxc file push ../kubeconfigs/admin.kubeconfig ${instance}/root/
   lxc file push ../kube-binaries/kubectl ${instance}/usr/local/bin/
 done
