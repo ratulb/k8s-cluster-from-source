47fa154461c69f20e78e541876b2eb088c8f0886
Ratul <ratul75@hotmail.com>
2020-10-12 05:43:18 +0530

    Update reboot-kubelet-boxes.sh

--- scripts/kubelet/reboot-kubelet-boxes.sh
+++ scripts/kubelet/reboot-kubelet-boxes.sh
@@ -5,11 +5,10 @@ WORKERS=
 if [ $# -eq 0 ];
   then
     WORKERS="worker-1 worker-2 worker-3"
-    echo "No arguments supplied - setting up for $WORKERS"
   else
     WORKERS=$@
-    echo "Setting up for $WORKERS"
 fi
+echo "Setting up for $WORKERS"
 for instance in $WORKERS; do
   lxc exec ${instance} reboot
 done
