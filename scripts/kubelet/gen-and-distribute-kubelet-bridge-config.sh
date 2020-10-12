3fee2b853065595d934eabd787e060fe73f7efb6
Ratul <ratul75@hotmail.com>
2020-10-12 05:41:38 +0530

    Update gen-and-distribute-kubelet-bridge-config.sh

--- scripts/kubelet/gen-and-distribute-kubelet-bridge-config.sh
+++ scripts/kubelet/gen-and-distribute-kubelet-bridge-config.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
 
 #Generate and copy the bridge and loopback onfiguration to worker nodes
-
 {
-
 WORKERS=
-
 if [ $# -eq 0 ];
 #!/usr/bin/env bash
   then
-#!/usr/bin/env bash
     WORKERS="worker-1 worker-2 worker-3"
-    echo "No arguments supplied - setting up for $WORKERS"
   else
     WORKERS=$@
-    echo "Setting up for $WORKERS"
 fi
-
+echo "Setting up for $WORKERS"
 GENERATED_DIR=./generated/etc/cni/net.d/
-
 mkdir -p ${GENERATED_DIR}
 
 #The loopback conf
-
 cat <<EOF | tee ${GENERATED_DIR}/99-loopback.conf
 {
     "cniVersion": "0.3.1",
@@ -31,15 +23,11 @@ cat <<EOF | tee ${GENERATED_DIR}/99-loopback.conf
 }
 EOF
 
-
 SEED_CIDR=10.200
 COUNTER=1
-
 for instance in $WORKERS; do
-
-POD_CIDR=${SEED_CIDR}.${COUNTER}.0/24
-
-cat <<EOF | tee ${GENERATED_DIR}/10-bridge.conf
+ POD_CIDR=${SEED_CIDR}.${COUNTER}.0/24
+ cat <<EOF | tee ${GENERATED_DIR}/10-bridge.conf
 {
     "cniVersion": "0.3.1",
     "name": "bridge",
@@ -59,18 +47,12 @@ EOF
 
 ((COUNTER++))
 
-#Push the generated file to the current worker node
-
 cp ${GENERATED_DIR}/10-bridge.conf ${GENERATED_DIR}/10-bridge.conf.${instance}
-
+#Push the generated file to the current worker node
 lxc exec ${instance} -- mkdir -p /etc/cni/net.d/
-
 lxc file push ${GENERATED_DIR}/10-bridge.conf ${instance}/etc/cni/net.d/
 lxc file push ${GENERATED_DIR}/99-loopback.conf ${instance}/etc/cni/net.d/
-
 echo "Bridge network/loopback configurations copied to ${instance}"
-
 done
-
 }
 
