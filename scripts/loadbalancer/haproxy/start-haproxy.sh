9299b806cf28a600dba519937eb67c6b3a1ddc57
Ratul <ratul75@hotmail.com>
2020-10-13 13:47:10 +0530

    Update start-haproxy.sh

--- scripts/loadbalancer/haproxy/start-haproxy.sh
+++ scripts/loadbalancer/haproxy/start-haproxy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 . ../../run-as-root.sh
+#Allow binding at port 80
+
+echo 'net.ipv4.ip_nonlocal_bind=1' >> /etc/sysctl.conf
+lxc exec loadbalancer -- sysctl -p
 lxc exec loadbalancer -- systemctl daemon-reload
 lxc exec loadbalancer -- systemctl stop haproxy
 lxc exec loadbalancer -- systemctl enable haproxy
